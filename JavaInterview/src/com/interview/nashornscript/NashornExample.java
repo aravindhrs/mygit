a3c7b6a6e42040d1c363d723343a08c0980b6686
aravindhrs <aravindhrs@outlook.com>
2017-05-13 03:04:16 +0530

    2nd change

--- JavaInterview/src/com/interview/nashornscript/NashornExample.java
+++ JavaInterview/src/com/interview/nashornscript/NashornExample.java
@@ -13,19 +13,22 @@ import javax.script.ScriptException;
 public class NashornExample {
 	public static void main(String[] args) {
 		try {
-			ScriptEngine engine = new ScriptEngineManager().getEngineByName("Nashorn");
-			Bindings bind = engine.getBindings(ScriptContext.ENGINE_SCOPE);		    
+			ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("Nashorn");
+			//engine = new ScriptEngineManager().getEngineByExtension("js");
+			Bindings bind = scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);		    
 		    bind.put("message", "Welcome to Javascript Nashorn");
 		    bind.put("name", "Aravindh");
-			Object script = engine.eval(new FileReader("D:\\GitHub\\JavaInterview\\nashron\\FirstNashron.js"));
+			Object script = scriptEngine.eval(new FileReader("D:\\GitHub\\JavaInterview\\nashron\\FirstNashron.js"));
 			System.out.println("Javascript:\n-----------\n"+script.toString()+"\n-------------");
-			Invocable invocable = (Invocable) engine;
-			
+			Invocable invocable = (Invocable) scriptEngine;			
 			invocable.invokeFunction("functionDemo1", "Nashorn");
 			Object result1 = invocable.invokeFunction("functionDemo3", "Nashorn","Secondtry");
 			System.out.println("Secondtry:"+result1);
 			Object result=invocable.invokeFunction("getMsg","NewTry");
 			System.out.println("New Try:"+result.toString());
+			System.out.println("-------------------------");
+			System.out.println("ScriptEngine.NAME:"+ScriptEngine.NAME+" ENGINE_VERSION:"+ScriptEngine.ENGINE_VERSION+" FILENAME:"+ScriptEngine.FILENAME);
+			System.out.println("ScriptEngine.LANGUAGE:"+ScriptEngine.LANGUAGE+" LANGUAGE_VERSION:"+ScriptEngine.LANGUAGE_VERSION+" ARGV:"+ScriptEngine.ARGV);
 		} catch (FileNotFoundException | ScriptException | NoSuchMethodException e) {
 			e.printStackTrace();
 		}
